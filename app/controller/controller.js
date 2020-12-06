421d572ee4b37af335b8a2a77a8e412b84513358
Akshay Easwaran <akeaswaran@me.com>
2020-12-06 17:32:49 -0500

    refactor to pull in ESPN data and clean it up a bit

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -1,52 +1,76 @@
+const cfb = require('cfb-data');
+const xgb = require('ml-xgboost');
+// const epModel = xgb.load('../models/ep-model.json');
+const reducer = (accumulator, currentValue) => accumulator.concat(currentValue);
+async function retrievePBP(req, res) {
+    // get game all game data
+    let summary = await cfb.games.getPlayByPlay(req.query.gameId);
+    
+    // strip out the unnecessary crap
+    delete summary.news;
+    delete summary.article;
+    delete summary.standings;
+    delete summary.videos;
+    delete summary.header;
+    delete summary.pickcenter; // could probably reuse the spread from here to give an initial prediction
+    delete summary.teams;
 
-const axios = require('axios');
-const cfbgames = require('../services/cfb.game.service')
+    summary.gameInfo = summary.competitions[0];
+    delete summary.competitions;
 
-exports.getPBP = async (id) => {
-    const baseUrl = 'http://cdn.espn.com/core/college-football/playbyplay';
-    const params = {
-        gameId: id,
-        xhr: 1,
-        render: 'false',
-        userab: 18
-    };
-    const res = await axios.get(baseUrl, {
-        params
-    });
-    return {
-        scoringPlays: res.data.gamepackageJSON.scoringPlays,
-        videos: res.data.gamepackageJSON.videos,
-        drives: res.data.gamepackageJSON.drives,
-        teams: res.data.gamepackageJSON.header.competitions[0].competitors,
-        id: res.data.gamepackageJSON.header.id,
-        competitions: res.data.gamepackageJSON.header.competitions,
-        season: res.data.gamepackageJSON.header.season,
-        week: res.data.gamepackageJSON.header.week
+    var drives = summary.drives.previous;
+    if ("current" in summary.drives) {
+        drives.concat(summary.drives.current)
     }
-},
-casesWithOutcome: (req, res) => {
-    const casesByDateUrl = 'https://www.worldometers.info/coronavirus/coronavirus-cases/'
-    axios(casesByDateUrl).then((result) => {
-        const response = utility.casesWithOutcome(result)
-        res.send(response)
-    })
-},
-currentlyInfected: (req, res) => {
-    const casesByDateUrl = 'https://www.worldometers.info/coronavirus/coronavirus-cases/'
-    axios(casesByDateUrl).then((result) => {
-        const response = utility.currentlyInfected(result)
-        res.send(response)
-    })
-},
-totalDeaths: (req, res) => {
-    const casesByDeathUrl = 'https://www.worldometers.info/coronavirus/coronavirus-death-toll/'
-    axios(casesByDeathUrl).then((result) => {
-        const response = utility.totalDeathCases(result)
-        res.send(response);
-    })
+    drives.sort((a,b) => parseInt(a.id) < parseInt(b.id))
 
+    var plays = drives.map(d => d.plays).reduce(reducer);
+    
+    if ("winprobability" in summary) {
+        summary.espnWP = summary.winprobability
+        delete summary.winprobability
+    }
+    
+    plays.sort((a, b) => parseInt(a.id) < parseInt(b.id));
+    plays.forEach(p => p.expectedPoints = calculateEPA(p));
+    plays.forEach(p => p.winProbability = calculateEPA(p));
+    summary.scoringPlays = plays.filter(p => ("scoringPlay" in p) && (p.scoringPlay == true))
+    summary.plays = plays;
+    summary.drives = drives;
+
+    return res.json(summary)
 }
 
+function calculateEPA(play) {
+    // INPUTS: ['Intercept', 'down2', 'down3', 'down4',
+    //    'goal_to_go', 'under_two', 'time_remaining',
+    //    'adjusted_yardline', 'adjusted_yardline_down_2',
+    //    'adjusted_yardline_down_3', 'adjusted_yardline_down_4',
+    //    'log_distance', 'log_distance_down_2',
+    //    'log_distance_down_3', 'log_distance_down_4',
+    //    'goal_to_go_log_distance']
 
 
+    return {
+        "before" : 0.0,
+        "after" : 0.0,
+        "added" : 0.0
+    }
+}
 
+function calculateWPA(play) {
+    // INPUTS: ["pos_team_receives_2H_kickoff","spread_time","TimeSecsRem","adj_TimeSecsRem","ExpScoreDiff_Time_Ratio",
+    // "pos_score_diff_start","down","distance","yards_to_goal","is_home","pos_team_timeouts_rem_before","def_pos_team_timeouts_rem_before",
+    // "period"]
+
+    // how do we figure out when timeouts are taken?
+    return {
+        "before" : 0.0,
+        "after" : 0.0,
+        "added" : 0.0
+    }
+}
+
+exports.getPBP = retrievePBP
+exports.calculateEPA = calculateEPA
+exports.calculateWPA = calculateWPA
--- app/routes/router.js
+++ app/routes/router.js
@@ -3,10 +3,7 @@ const Controller = require('../controller/controller');
 
 const router = express.Router();
 
-
 router.get('/cfb/pbp', Controller.getPBP);
-router.get('/cfb/box', Controller.getBox);
-router.get('/cfb/summary', Controller.getSummary)
-router.get('/currentlyInfected', Controller.currentlyInfected)
+
 
 module.exports = router;
