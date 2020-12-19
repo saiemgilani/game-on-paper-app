58a2ed5ab7048ab20a5b6528937ca87c4855893d
Saiem Gilani <saiem.gilani@gmail.com>
2020-12-19 13:28:27 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes/router');
 const morgan = require("morgan");
-const port = 5000
+const port = 5000;
 
 const app = express();
 app.use(morgan('[api] :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
