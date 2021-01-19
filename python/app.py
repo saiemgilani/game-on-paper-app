61f7781ac14c1313f1b3558919a35c07e66de7ad
Saiem Gilani <saiem.gilani@gmail.com>
2021-01-19 09:15:22 -0500

    Update app.py
    change output start/end structure a little

--- python/app.py
+++ python/app.py
@@ -40,12 +40,15 @@ def process():
     base_data = request.get_json(force=True)['data']
     drives_data = request.get_json(force=True)['drivesData']
     boxScore = request.get_json(force=True)['boxScore']
+    season = request.get_json(force=True)['season']
     spread = request.get_json(force=True)['homeTeamSpread']
     homeTeam = request.get_json(force=True)['homeTeamId']
+    homeName = request.get_json(force=True)['homeTeamName']
     awayTeam = request.get_json(force=True)['awayTeamId']
+    awayName = request.get_json(force=True)['awayTeamName']
     firstHalfKickoffTeam = request.get_json(force=True)['firstHalfKickoffTeamId']
 
-    processed_data = PlayProcess(logger = logging.getLogger("root"), json_data=base_data, drives_data=drives_data, boxScore = boxScore, spread=spread, homeTeam=homeTeam, awayTeam=awayTeam, firstHalfKickoffTeam=firstHalfKickoffTeam)
+    processed_data = PlayProcess(logger = logging.getLogger("root"), json_data=base_data, drives_data=drives_data, boxScore = boxScore, season = season,  spread=spread, homeTeam=homeTeam, homeName=homeName, awayTeam=awayTeam, awayName=awayName, firstHalfKickoffTeam=firstHalfKickoffTeam)
     processed_data.run_processing_pipeline()
     tmp_json = processed_data.plays_json.to_json(orient="records")
     jsonified_df = json.loads(tmp_json)
@@ -54,10 +57,11 @@ def process():
     
     bad_cols = [
         'start.distance', 'start.yardLine', 'start.team.id', 'start.down', 'start.yardsToEndzone', 'start.posTeamTimeouts', 'start.defTeamTimeouts', 
-        'start.shortDownDistanceText', 'start.possessionText', 'start.downDistanceText',
+        'start.shortDownDistanceText', 'start.possessionText', 'start.downDistanceText', 'start.pos_team_timeouts', 'start.def_pos_team_timeouts',
         'clock.displayValue', 
-        'type.id', 'type.text', 'type.abbreviation'
-        'end.shortDownDistanceText', 'end.possessionText', 'end.downDistanceText', 'end.distance', 'end.yardLine', 'end.team.id','end.down', 'end.yardsToEndzone', 'end.posTeamTimeouts','end.defTeamTimeouts', 
+        'type.id', 'type.text', 'type.abbreviation',
+        'end.distance', 'end.yardLine', 'end.team.id','end.down', 'end.yardsToEndzone', 'end.posTeamTimeouts','end.defTeamTimeouts', 
+        'end.shortDownDistanceText', 'end.possessionText', 'end.downDistanceText', 'end.pos_team_timeouts', 'end.def_pos_team_timeouts',
         'expectedPoints.before', 'expectedPoints.after', 'expectedPoints.added', 
         'winProbability.before', 'winProbability.after', 'winProbability.added', 
         'scoringType.displayName', 'scoringType.name', 'scoringType.abbreviation'
@@ -90,12 +94,27 @@ def process():
             "team" : {
                 "id" : record["start.team.id"],
             },
+            "pos_team": {
+                "id" : record["start.pos_team.id"],
+            }, 
+            "def_pos_team": {
+                "id" : record["start.def_pos_team.id"],
+            }, 
             "distance" : record["start.distance"],
             "yardLine" : record["start.yardLine"],
             "down" : record["start.down"],
             "yardsToEndzone" : record["start.yardsToEndzone"],
+            "homeScore" : record["start.homeScore"],
+            "awayScore" : record["start.awayScore"],
+            "pos_team_score" : record["start.pos_team_score"],
+            "def_pos_team_score" : record["start.def_pos_team_score"],
+            "pos_score_diff" : record["start.pos_score_diff"],
             "posTeamTimeouts" : record["start.posTeamTimeouts"],
             "defTeamTimeouts" : record["start.defTeamTimeouts"],
+            "pos_team_timeouts" : record["start.pos_team_timeouts"],
+            "def_pos_team_timeouts" : record["start.def_pos_team_timeouts"],
+            "ExpScoreDiff" : record["start.ExpScoreDiff"],
+            "ExpScoreDiff_Time_Ratio" : record["start.ExpScoreDiff_Time_Ratio"],
             "shortDownDistanceText" : record["start.shortDownDistanceText"],
             "possessionText" : record["start.possessionText"],
             "downDistanceText" : record["start.downDistanceText"]
@@ -105,12 +124,27 @@ def process():
             "team" : {
                 "id" : record["end.team.id"],
             },
+            "pos_team": {
+                "id" : record["end.pos_team.id"],
+            }, 
+            "def_pos_team": {
+                "id" : record["end.def_pos_team.id"],
+            }, 
             "distance" : record["end.distance"],
             "yardLine" : record["end.yardLine"],
             "down" : record["end.down"],
             "yardsToEndzone" : record["end.yardsToEndzone"],
+            "homeScore" : record["end.homeScore"],
+            "awayScore" : record["end.awayScore"],
+            "pos_team_score" : record["end.pos_team_score"],
+            "def_pos_team_score" : record["end.def_pos_team_score"],
+            "pos_score_diff" : record["end.pos_score_diff"],
             "posTeamTimeouts" : record["end.posTeamTimeouts"],
             "defTeamTimeouts" : record["end.defTeamTimeouts"],
+            "pos_team_timeouts" : record["end.pos_team_timeouts"],
+            "def_pos_team_timeouts" : record["end.def_pos_team_timeouts"],
+            "ExpScoreDiff" : record["end.ExpScoreDiff"],
+            "ExpScoreDiff_Time_Ratio" : record["end.ExpScoreDiff_Time_Ratio"],
             "shortDownDistanceText" : record["end.shortDownDistanceText"],
             "possessionText" : record["end.possessionText"],
             "downDistanceText" : record["end.downDistanceText"]
