00bc46b3f7c3cf94e46e7592916afb75414f86cf
Akshay Easwaran <akeaswaran@me.com>
2021-09-05 21:23:58 -0400

    update to be total EPA

--- frontend/public/assets/js/dashboard.js
+++ frontend/public/assets/js/dashboard.js
@@ -214,8 +214,8 @@ if (gameData.plays.length > 0) {
 
     var homeTeamWP = gameData.plays.map(p => (p.pos_team == homeTeam.id) ? translateWP(p.winProbability.before) : translateWP(1.0 - p.winProbability.before));
 
-    var homeTeamEPA = calculateCumulativeSums(gameData.plays.filter(p => (p.pos_team == homeTeam.id && p.play)).map(p => p.expectedPoints.added)).map((p, idx) => { return { "x": (idx + 1), "y": p } });
-    var awayTeamEPA = calculateCumulativeSums(gameData.plays.filter(p => (p.pos_team == awayTeam.id && p.play)).map(p => p.expectedPoints.added)).map((p, idx) => { return { "x": (idx + 1), "y": p } });
+    var homeTeamEPA = calculateCumulativeSums(gameData.plays.filter(p => (p.pos_team == homeTeam.id)).map(p => p.expectedPoints.added)).map((p, idx) => { return { "x": (idx + 1), "y": p } });
+    var awayTeamEPA = calculateCumulativeSums(gameData.plays.filter(p => (p.pos_team == awayTeam.id)).map(p => p.expectedPoints.added)).map((p, idx) => { return { "x": (idx + 1), "y": p } });
 
     homeTeamEPA.splice(0, 0, {
         x: 0,
@@ -538,7 +538,7 @@ if (gameData.plays.length > 0) {
                         yAxes: [{
                             scaleLabel: {
                                 display: true,
-                                labelString: "Total Offensive EPA"
+                                labelString: "Total EPA"
                             },
                             gridLines: gridLines
                         }],
