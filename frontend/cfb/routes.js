8823dc79f71574d91d5cbe5d65924d1c5546af7d
Akshay Easwaran <akeaswaran@me.com>
2021-10-28 20:59:02 -0400

    update sort style

--- frontend/cfb/routes.js
+++ frontend/cfb/routes.js
@@ -19,21 +19,27 @@ async function retrieveGameList(url, params) {
         return (parseFloat(homeComp.id) >= 0 && parseFloat(awayComp.id) >= 0);
     })
     gameList.sort((a, b) => {
-        var aDate = Date.parse(a.date)
-        var bDate = Date.parse(b.date)
-        if (aDate < bDate) {
-            return -1
-        } else if (aDate > bDate) {
-            return 1
+        if (a.status.type.name.includes("IN_PROGRESS") && !b.status.type.name.includes("IN_PROGRESS")) {
+            return -1;
+        } else if (b.status.type.name.includes("IN_PROGRESS") && !a.status.type.name.includes("IN_PROGRESS")) {
+            return 1;
         } else {
-            var aVal = parseInt(a.status.type.id)
-            var bVal = parseInt(b.status.type.id)
-            if (aVal > bVal) {
+            var aDate = Date.parse(a.date)
+            var bDate = Date.parse(b.date)
+            if (aDate < bDate) {
                 return -1
-            } else if (aVal < bVal) {
+            } else if (aDate > bDate) {
                 return 1
             } else {
-                return 0
+                var aVal = parseInt(a.status.type.id)
+                var bVal = parseInt(b.status.type.id)
+                if (aVal > bVal) {
+                    return -1
+                } else if (aVal < bVal) {
+                    return 1
+                } else {
+                    return 0
+                }
             }
         }
     })
