00ef24eaab0202e829b4a01d3b6352052c645cf5
Akshay Easwaran <akeaswaran@me.com>
2023-08-27 14:52:17 -0400

    update switcher for 2023

---
+++ update_calendar.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+import json
+
+base_url = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard"
+
+
+def get_calendar(year: int):
+    j = requests.get(f"{base_url}?dates={year}0901")
+    calendar = j.json()['leagues'][0]['calendar']
+
+    weeks = []
+    for t in calendar:
+        if t["value"] == "4":
+            continue
+
+        for e in t['entries']:
+            e['year'] = f"{year}"
+            weeks.append(e)
+    result = {
+        f"{year}" : weeks
+    }
+
+    print(json.dumps(result))
+
+# update this to any arbitrary year and add it to frontend/cfb/schedule.json to update the weeks available in the switcher.
+get_calendar(2023)
