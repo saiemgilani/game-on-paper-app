79a576de8501c7fff85a26c916d344553640b6a9
Akshay Easwaran <akeaswaran@me.com>
2025-10-19 18:09:09 -0400

    update logo for da U

--- frontend/public/assets/js/epa_chart.js
+++ frontend/public/assets/js/epa_chart.js
@@ -1,6 +1,7 @@
 const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const specialImages = {
     "61": "/assets/img/ennui-uga.png",
+    "2390": "/assets/img/upside-down-u.png",
 };
 const imageSize = 37.5;
 
