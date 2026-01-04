bee59008906cb220a5040c2d3cbc331554621271
Akshay Easwaran <akeaswaran@me.com>
2026-01-03 20:42:18 -0500

    add better wait

--- load_test.sh
+++ load_test.sh
@@ -2,11 +2,17 @@
 docker compose stop
 docker compose up --build -d
 
-wait 15 # wait for the app to come up
+echo "Waiting for the app to come up..."
+sleep 5 # wait for the app to come up
 
+echo "Spinning up N Chrome instances..."
 for i in {1..15}
 do
  open -na "Google Chrome" --args -incognito "http://localhost:8000/cfb/game/401778302"
 done
 
-# docker compose stop
+echo "Waiting for all Chrome instances to load..."
+sleep 5 # wait for the app to come up
+
+echo "Test over, killing app"
+docker compose stop
