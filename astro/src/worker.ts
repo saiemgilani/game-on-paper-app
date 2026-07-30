fdc38475d75c14fab2adbdf04fdfe163b94800a3
Akshay Easwaran <akeaswaran@me.com>
2026-07-30 09:36:57 -0400

    disable crons for now

--- astro/src/worker.ts
+++ astro/src/worker.ts
@@ -7,15 +7,15 @@ export default {
     async fetch(request, env, ctx) {
         return handle(request, env, ctx);
     },
-    async scheduled(controller, env, ctx) {
-        switch (controller.cron || "*/2 * * * *") {
-            case "*/2 * * * *":
-                try {
-                    await getCurrentScoreboard(false, true);
-                } catch (err) {
-                    console.error(`ERROR while reloading scoreboard cache: ${err}`)
-                }
-                break;
-        }
-    },
+    // async scheduled(controller, env, ctx) {
+    //     switch (controller.cron || "*/2 * * * *") {
+    //         case "*/2 * * * *":
+    //             try {
+    //                 await getCurrentScoreboard(false, true);
+    //             } catch (err) {
+    //                 console.error(`ERROR while reloading scoreboard cache: ${err}`)
+    //             }
+    //             break;
+    //     }
+    // },
 } satisfies ExportedHandler<Env>;
