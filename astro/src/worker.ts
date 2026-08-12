6d73331aa0576e7086256c46592b3d7adf345908
Akshay Easwaran <akeaswaran@me.com>
2026-08-12 17:33:27 -0400

    enable bg crons

--- astro/src/worker.ts
+++ astro/src/worker.ts
@@ -7,15 +7,16 @@ export default {
     async fetch(request, env, ctx) {
         return handle(request, env, ctx);
     },
-    // async scheduled(controller, env, ctx) {
-    //     switch (controller.cron || "*/2 * * * *") {
-    //         case "*/2 * * * *":
-    //             try {
-    //                 await getCurrentScoreboard(false, true);
-    //             } catch (err) {
-    //                 console.error(`ERROR while reloading scoreboard cache: ${err}`)
-    //             }
-    //             break;
-    //     }
-    // },
+    async scheduled(controller, env, ctx) {
+        // NOTE: offseason config
+        switch (controller.cron || "0 0 * * *") {
+            case "0 0 * * *":
+                try {
+                    await getCurrentScoreboard(false, true);
+                } catch (err) {
+                    console.error(`ERROR while reloading scoreboard cache: ${err}`)
+                }
+                break;
+        }
+    },
 } satisfies ExportedHandler<Env>;
