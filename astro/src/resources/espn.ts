1816133cc0aee64845c00f65486c92594d1b38b5
Akshay Easwaran <akeaswaran@me.com>
2026-08-27 21:51:46 -0400

    fix overlapping game states

--- astro/src/resources/espn.ts
+++ astro/src/resources/espn.ts
@@ -328,7 +328,7 @@ export interface ESPNPlayByPlayResponse {
     }
 }
 export const ESPN_IN_PROGRESS_STATUS_NAMES = ["STATUS_IN_PROGRESS", "STATUS_END_PERIOD", "STATUS_HALFTIME", "STATUS_DELAYED"]
-export const ESPN_INVALID_GAME_STATUS_NAMES = ["STATUS_ABANDONED", "STATUS_POSTPONED", "STATUS_DELAYED"]
+export const ESPN_INVALID_GAME_STATUS_NAMES = ["STATUS_ABANDONED", "STATUS_POSTPONED"]
 
 async function requestESPN(url: string, init?: RequestInit): Promise<Response> {
     return await wrappedFetch(
