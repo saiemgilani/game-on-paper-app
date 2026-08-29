5290b07197b9ef77b4ab49a0fae48cb680133bbb
Akshay Easwaran <akeaswaran@me.com>
2026-08-29 12:28:07 -0400

    scoreboard: add cache buster

--- astro/src/resources/espn.ts
+++ astro/src/resources/espn.ts
@@ -448,7 +448,7 @@ export async function getCurrentScoreboard(cacheReadEnabled = true, cacheWriteEn
 
         console.info(`ESPN API cache miss (cacheWriteEnabled: ${cacheWriteEnabled}): scoreboard`)
         // thanks to @pseudo-r on GitHub: https://github.com/pseudo-r/Public-ESPN-API#core-api-v3-enriched-schema
-        const resp = await requestESPN(`https://cdn.espn.com/core/college-football/scoreboard?group=80&limit=1000&xhr=1`)
+        const resp = await requestESPN(`https://cdn.espn.com/core/college-football/scoreboard?group=80&limit=1000&xhr=1&${(new Date()).getTime()}`)
 
         if (!resp.ok) {
             throw new Error(`ESPN API: scoreboard request received ${resp.statusText}`)
