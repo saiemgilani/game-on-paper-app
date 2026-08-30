c277d446c6253fe7085d8e8770d503f56b56470b
saiemgilani <saiem.gilani@gmail.com>
2026-08-30 00:06:29 -0400

    style(leaderboards): keep the three metric dropdowns on one line at lg+

--- astro/src/components/dropdowns/PlayerMetricDropdown.svelte
+++ astro/src/components/dropdowns/PlayerMetricDropdown.svelte
@@ -22,7 +22,7 @@
 
 </script>
 <form class="mb-3 d-flex justify-content-lg-end justify-content-xs-start">
-    <div class="row">
+    <div class="row flex-lg-nowrap">
         <div class="col-lg-auto mb-3">
             <select class="form-select form-select-md" onchange={onChangeSeason}>
 				<option value="-1" disabled>Choose Season...</option>
